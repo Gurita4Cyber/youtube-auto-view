d1d06d958d71b9a07eba71ea51a690f828ab095f
unknown <student.linuxs@gmail.com>
2020-07-01 11:22:57 +0700

    tes

---
+++ autorefresh/index.php
@@ -0,0 +1,28 @@
+
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="HandheldFriendly" content="true" />
+<meta name="apple-touch-fullscreen" content="yes" />
+<link rel="stylesheet" href="inc/style.css" type="text/css" media="screen">
+
+
+
+<div class="autorefresh">
+  <ul>
+    
+	<!-- column 1 -->
+    <li>
+		<div class='embed-container'><iframe src='https://www.youtube.com/embed/fEmF014zH0I?autoplay=1' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen allowfullscreen></iframe></div>
+	</li>
+	<!-- end column 1 -->
+    
+  </ul>
+</div>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+   window.jQuery || document.write('<script src="inc/jquery.js"><\/script>');
+</script>
+<script src="inc/script.js"></script>
